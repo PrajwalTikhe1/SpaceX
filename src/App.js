8ddeabcbe967b3923eace4c16d7d9dc1c9a843bc
PrajwalTikhe1 <prajwaltikhesrt4@gmail.com>
2021-11-30 10:22:09 +0530

    Created react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      message: 'Hello react',
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.spacexdata.com/v3/launches')
+      .then((response) => response.json())
+      .then((rockets) => console.log(rockets));
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>{this.state.message}</p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+          <button onClick={() => this.setState({ message: 'Hello world!' })}>
+            Click Me
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
