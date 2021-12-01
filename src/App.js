48c84b1d2cf2bfb86e40ca4a79249ebd5f3929c6
PrajwalTikhe1 <prajwaltikhesrt4@gmail.com>
2021-12-01 17:54:07 +0530

    Filter component added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { Component, Fragment } from 'react';
 import './App.css';
 import { Card } from './components/Card';
 import { Modal } from './components/Modal';
-// import { LaunchYearFilter } from './components/filters/LaunchYearFilter';
-// import { LaunchStatusFilter } from './components/filters/LaunchStatusFilter';
-// import { UpcomingFilter } from './components/filters/UpcomingFilter';
+import { LaunchYearFilter } from './components/filters/LaunchYearFilter';
+import { LaunchStatusFilter } from './components/filters/LaunchStatusFilter';
+import { UpcomingFilter } from './components/filters/UpcomingFilter';
 
 class App extends Component {
   constructor() {
@@ -13,9 +13,9 @@ class App extends Component {
     this.state = {
       flights: [],
       searchFlights: '',
-      // selectedYear: 0,
-      // selectedStatus: 0,
-      // selectedUpcoming: 0,
+      selectedYear: 0,
+      selectedStatus: 0,
+      selectedUpcoming: 0,
     };
   }
 
@@ -28,26 +28,17 @@ class App extends Component {
   handleSearchFlights = (e) => {
     this.setState({ searchFlights: e.target.value });
   };
-
-  // handleChangeYear = (e) => {
-  //   this.setState({ selectedYear: e.target.value });
-  // };
-  // handleChangeStatus = (e) => {
-  //   this.setState({ selectedStatus: e.target.value });
-  // };
-  // handleChangeUpcoming = (e) => {
-  //   this.setState({ selectedUpcoming: e.target.value });
-  // };
+  handleChangeYear = (e) => {
+    this.setState({ selectedYear: e.target.value });
+  };
+  handleChangeStatus = (e) => {
+    this.setState({ selectedStatus: e.target.value });
+  };
+  handleChangeUpcoming = (e) => {
+    this.setState({ selectedUpcoming: e.target.value });
+  };
 
   render() {
-    // const { flights, selectedYear, selectedStatus, selectedUpcoming } =
-    //   this.state;
-    // const filteredFlights = flights.filter(
-    //   (flight) =>
-    //     flight.launch_year > selectedYear ||
-    //     flight.launch_status > selectedStatus ||
-    //     flight.launch_upcoming > selectedUpcoming
-    // );
     const { flights, searchFlights } = this.state;
     const filteredFlights = flights.filter((flight) => {
       if (searchFlights === '') {
@@ -68,11 +59,11 @@ class App extends Component {
           <input
             className='searchbar'
             type='text'
-            placeholder='Search flights...'
+            placeholder='Search flight missions...'
             onChange={this.handleSearchFlights}
           />
         </div>
-        {/* <div className='row justify-content-center'>
+        <div className='row justify-content-center'>
           <div className='col-md-2 col-md-offset-3'>
             <LaunchYearFilter onChange={this.handleChangeYear} />
           </div>
@@ -82,7 +73,7 @@ class App extends Component {
           <div className='col-md-2'>
             <UpcomingFilter onChange={this.handleChangeUpcoming} />
           </div>
-        </div> */}
+        </div>
         <div className='row'>
           {filteredFlights.map((flight) => (
             <Fragment>
